b40ce777f3bbb146213f7233355f1356584cfa6a
SamirKhan21860 <msamirkhan561@gmail.com>
2024-02-18 12:29:07 +0500

    YouTube.jsx added and import into App.jsx

--- vite-pro/src/App.jsx
+++ vite-pro/src/App.jsx
@@ -1,6 +1,13 @@
+import YouTube from "./Youtube";
+
 function App() {
   return (
-    <h1>Vite react app</h1>
+    <>
+      <h1>Vite react app</h1>
+      <h1>Vite react app</h1>
+      <YouTube/>
+    </>
+    
   )
 }
 
