c939746184bf73fda73ab8baf7b0b9e4dfc11532
SamirKhan21860 <msamirkhan561@gmail.com>
2024-02-18 15:26:21 +0500

    main.js has some changes to understand how react works comparing with js

--- vite-pro/src/main.jsx
+++ vite-pro/src/main.jsx
@@ -1,10 +1,40 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+
+const reactElementObject = {
+  type: 'a',
+  props: {
+      href: "https://google.com",
+      target: "_blank"
+  },
+  children: "Click me to visit google"
+}
+
+function MyApp() {
+  return (
+      <div>
+          <h1>Custom React app</h1>
+      </div>
+  )
+}
+
+const AnotherElement = (
+  <a href="http://google.com">Google Visit</a>
+)
+
+const areactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: "_blank"},
+  'Click to visit google'
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <App />
-
+  // <MyApp />
+  // AnotherElement 
+  // areactElement
 )
 
 
