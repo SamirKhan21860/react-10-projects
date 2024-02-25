78e8e00bc0161d227199c2fd2e3f5289a3e510f6
SamirKhan21860 <msamirkhan561@gmail.com>
2024-02-25 16:03:11 +0500

    card component have props

--- tailwindprops/src/components/Card.jsx
+++ tailwindprops/src/components/Card.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import image from "./img/hitesh.jpg";
 
-function Card() {
+function Card({username = " HC ", post = "Not assigned yet"}) {
+    // console.log(props);
     return (
         <div>
             <figure className="md:flex md:p-0 bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
                 <img className="w-24 h-24 rounded-full mx-auto md:rounded-none md:w-48 md:h-auto" src={image} alt="Hitesh" width="384" height="512" />
-                <div className="pt-6 text-center md:text-left space-y-4">
+                <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
                     <blockquote>
                         <p className="text-lg font-medium">
                             “Tailwind CSS is the only framework that I've seen scale
@@ -15,11 +16,9 @@ function Card() {
                         </p>
                     </blockquote>
                     <figcaption className="font-medium">
-                        <div className="text-sky-500 dark:text-sky-400">
-                            Sarah Dayan
-                        </div>
+                        <div className="text-sky-500 dark:text-sky-400">{username}</div>
                         <div className="text-slate-700 dark:text-slate-500">
-                            Staff Engineer, Algolia
+                            {post}
                         </div>
                     </figcaption>
                 </div>
