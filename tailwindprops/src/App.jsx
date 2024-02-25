b8cf063b2f29eea4a5698baf703e9b9fd8cfe7e4
SamirKhan21860 <msamirkhan561@gmail.com>
2024-02-25 13:21:23 +0500

    tailwind css docs example of card has been add

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -1,16 +1,13 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Card from './components/Card';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
       <h1 className='bg-green-400 text-3xl p-3 rounded-md'>Vite with Tailwind</h1>
+      <Card />
     </>
   )
 }
 
-export default App
+export default App;
