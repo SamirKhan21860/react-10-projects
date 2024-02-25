8970266db4c7f54a0fac615a6f4fb40fbb6e881c
SamirKhan21860 <msamirkhan561@gmail.com>
2024-02-25 17:49:18 +0500

    all set and working expect props red underlined

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <>
       <h1 className='bg-green-400 text-3xl p-3 rounded-md'>Vite with Tailwind</h1>
-      <Card username="Sameer Khan" />
+      <Card username="Sameer Khan" post="Front End Developer"/>
       <Card username='Json' post="Staff Engineer" />
-      <Card myArr={[1, 2, 3]} />
+      <Card />
     </>
   )
 }
