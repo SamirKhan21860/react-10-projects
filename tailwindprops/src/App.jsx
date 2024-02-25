8a0f3099576f9de280c75ac02471444bd333c6bb
SamirKhan21860 <msamirkhan561@gmail.com>
2024-02-25 18:46:56 +0500

    in tailwindprop we understand about props

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -2,12 +2,25 @@ import './App.css';
 import Card from './components/Card';
 
 function App() {
+
+  let myObj = {
+    name: 'Hitesh',
+    age: 25,
+    address: {
+        city: 'Bengaluru',
+        state: 'Karnataka',
+        country: 'India'
+    }
+  }
+
+  let newArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
   return (
     <>
       <h1 className='bg-green-400 text-3xl p-3 rounded-md'>Vite with Tailwind</h1>
       <Card username="Sameer Khan" post="Front End Developer"/>
       <Card username='Json' post="Staff Engineer" />
-      <Card />
+      <Card myArray={newArray}/>
     </>
   )
 }
