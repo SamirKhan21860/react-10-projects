87c18272e927c089dbb6ade5852b763013d879b8
SamirKhan21860 <msamirkhan561@gmail.com>
2024-03-01 17:26:32 +0500

    creating a InputBox component, Break time

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import InputBox from './../src/components/InputBox'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const logo = {
+    backgroundImage: `url(https://images.pexels.com/photos/106152/euro-coins-currency-money-106152.jpeg?auto=compress&cs=tinysrgb&w=300)`,
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat" style={logo}>
+        {/* <h1 className="bg-red-200">Test for tailwind</h1> */}
+        <InputBox />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
