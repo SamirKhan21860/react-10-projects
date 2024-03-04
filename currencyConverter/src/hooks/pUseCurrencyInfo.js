ee811065c124464b262d57b6c312c1ac78fe5e49
SamirKhan21860 <msamirkhan561@gmail.com>
2024-03-04 11:40:32 +0500

    I'm trying to add async function in pUseCurrencyInfo.js to await function until we get the data

---
+++ currencyConverter/src/hooks/pUseCurrencyInfo.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({})
+    useEffect(() => {
+        async function fetchData(currency) {
+            await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+            .then((response) => response.json())
+            .then((response) => setData(response[currency]))
+            .catch((e) => console.log(e))
+        }
+        fetchData(currency)
+    }, [currency])
+
+    return data
+}
+
+export default useCurrencyInfo
