493b61af8922e8b3095d2640a57ca3ac96070be3
SamirKhan21860 <msamirkhan561@gmail.com>
2024-03-06 12:47:29 +0500

    margin of header in nav is all set

--- d_react_Router/src/components/Header/Header.jsx
+++ d_react_Router/src/components/Header/Header.jsx
@@ -18,12 +18,12 @@ const Header = () => {
             <div className="flex items-center lg:order-2">
               <Link
                 className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                to="#"
+                to="/login"
               >
                 Log In
               </Link>
               <Link
-                to="#"
+                to="/getstarted"
                 className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
               >
                 Get Started
@@ -33,7 +33,7 @@ const Header = () => {
               id="mobile-menu-2"
               className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
             >
-              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-0 lg:mt-0">
+              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                 <li>
                   <NavLink
                     to="/"
@@ -97,11 +97,13 @@ const Header = () => {
                 <li>
                   <NavLink
                     to="/user"
-                    className={({ isActive }) =>
+                    className={({ isActive, isPending }) =>
                       `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
                                         ${isActive
                         ? "text-orange-700"
-                        : "text-gray-700"
+                        : isPending
+                          ? "text-gray-700"
+                          : ""
                       } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                     }
                   >
