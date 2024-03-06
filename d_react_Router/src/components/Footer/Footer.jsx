a73a6bc88612d8674ea49940d04b3365d1a5fbb0
SamirKhan21860 <msamirkhan561@gmail.com>
2024-03-06 17:03:23 +0500

    footer is all set

--- d_react_Router/src/components/Footer/Footer.jsx
+++ d_react_Router/src/components/Footer/Footer.jsx
@@ -1,56 +1,50 @@
-// import React from 'react'
-import { Link } from "react-router-dom";
+// import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Footer() {
+export default function Footer() {
   return (
-    <>
-      <footer className="bg-white border-y">
-        <div className="max-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-          <div className="md:flex md:justify-between">
-            <div className="mb-6 md:mb-0">
-              <Link to="/" className="flex items-center">
-                <img
-                  className="mr-3 h-16"
-                  src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                  alt="Logo"
-                />
-              </Link>
+    <footer className="bg-white border-y">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <Link to="/" className="flex items-center">
+              <img
+                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                className="mr-3 h-16"
+                alt="Logo"
+              />
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
+              <ul className="text-gray-500 font-medium">
+                <li className="mb-4">
+                  <Link to="/" className="hover:underline">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/about" className="hover:underline">
+                    About
+                  </Link>
+                </li>
+              </ul>
             </div>
-            <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-              <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                  Resources
-                </h2>
-                <ul className="text-gray-500 font-medium">
-                  <li className="mb-4">
-                    <Link to="/" className="hover:underline">
-                      Home
-                    </Link>
-                  </li>
-                  <li className="mb-4">
-                    <Link to="/about" className="hover:underline">
-                      About
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="">
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                Follow us
-              </h2>
-              <ul className="">
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
+              <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/SamirKhan21860"
+                    href="https://github.com/hiteshchoudhary"
                     className="hover:underline"
                     target="_blank"
-                    rel="nonreferrer"
+                    rel="noreferrer"
                   >
                     Github
                   </a>
                 </li>
-                <li className="mb-4">
+                <li>
                   <Link to="/" className="hover:underline">
                     Discord
                   </Link>
@@ -61,10 +55,14 @@ function Footer() {
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
-                  <Link to="#" className="hover:underline">Privacy Policy</Link>
+                  <Link to="#" className="hover:underline">
+                    Privacy Policy
+                  </Link>
                 </li>
                 <li>
-                  <Link to="#" className="hover:underline">Term &amp; Conditions</Link>
+                  <Link to="#" className="hover:underline">
+                    Terms &amp; Conditions
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -72,11 +70,14 @@ function Footer() {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center">&copy; 2023
-            <a href="https://google.com/" className="hover:underline">hiteshchoudhary</a>
+          <span className="text-sm text-gray-500 sm:text-center">
+            © 2023
+            <a href="https://hiteshchoudhary.com/" className="hover:underline">
+              hiteshchoudhary
+            </a>
             . All Rights Reserved.
           </span>
-          <div className="flex space-x-5 sm:justify-center sm:mt-0">
+          <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
             <Link to="#" className="text-gray-500 hover:text-gray-900">
               <svg
                 className="w-4 h-4"
@@ -135,7 +136,7 @@ function Footer() {
                   clip-rule="evenodd"
                 />
               </svg>
-              <span className="sr-only">Github account</span>
+              <span className="sr-only">GitHub account</span>
             </Link>
             <Link to="#" className="text-gray-500">
               <svg
@@ -151,13 +152,11 @@ function Footer() {
                   clip-rule="evenodd"
                 />
               </svg>
-              <span className="sr-only">Dribble account</span>
+              <span className="sr-only">Dribbble account</span>
             </Link>
           </div>
         </div>
-      </footer>
-    </>
+      </div>
+    </footer>
   );
 }
-
-export default Footer;
