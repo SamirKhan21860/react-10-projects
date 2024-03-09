bc24ed36dddcc50f6362f9a8167e03d9838dd34b
SamirKhan21860 <msamirkhan561@gmail.com>
2024-03-09 19:46:30 +0500

    contact is under styling ahan

--- d_react_Router/src/components/Contact/Contact.jsx
+++ d_react_Router/src/components/Contact/Contact.jsx
@@ -3,7 +3,31 @@
 const Contact = () => {
     return (
         <div>
-            Contact
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <div>
+                                <h1>Get in touch:</h1>
+                                <p>Fill in the form to start a conversation</p>
+                                <div className="flex items-center mt-8 text-gray-600">
+                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0 0 24 24" className="w-8 h-8 text-gray-500">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                                        <path stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="1.5"
+                                            d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                                        />
+                                    </svg>
+                                    <div className="ml-4 text-md">
+                                    Acme Inc, Street, State, Postal Code
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
