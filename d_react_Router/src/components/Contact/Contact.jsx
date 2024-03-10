67e4eb8e248b35c241552888b766947b868735a4
SamirKhan21860 <msamirkhan561@gmail.com>
2024-03-10 12:27:04 +0500

    Some styling, laying out the the contact page or route

--- d_react_Router/src/components/Contact/Contact.jsx
+++ d_react_Router/src/components/Contact/Contact.jsx
@@ -2,26 +2,24 @@
 
 const Contact = () => {
     return (
-        <div>
-            <div>
-                <div>
-                    <div>
-                        <div>
-                            <div>
-                                <h1>Get in touch:</h1>
-                                <p>Fill in the form to start a conversation</p>
-                                <div className="flex items-center mt-8 text-gray-600">
-                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0 0 24 24" className="w-8 h-8 text-gray-500">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                                        <path stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            stroke-width="1.5"
-                                            d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                                        />
-                                    </svg>
-                                    <div className="ml-4 text-md">
+        <div className="relative flex items-top justify-center min-h-[700px] bg-white sm:items-center sm:pt-0">
+            <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div className="mt-8 overflow-hidden">
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                        <div className="p-6 mr-2 bg-gray-100 sm:rounded-lg">
+                            <h1 className="text-3xl sm:text-4xl text-gray-800 font-extrabold tracking-tight">Get in touch:</h1>
+                            <p>Fill in the form to start a conversation</p>
+                            <div className="flex items-center mt-8 text-gray-600">
+                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0 0 24 24" className="w-8 h-8 text-gray-500">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                                    <path stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="1.5"
+                                        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                                    />
+                                </svg>
+                                <div className="ml-4 text-md">
                                     Acme Inc, Street, State, Postal Code
-                                    </div>
                                 </div>
                             </div>
                         </div>
